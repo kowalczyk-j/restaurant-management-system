7243aa5f135813084663f0e228c42d3bc0f9681d
Magdalena Dudek <gilva.evi@gmail.com>
2022-05-27 10:42:02 +0200

    modified pantry; added read from file method(csv), getters and setters

--- src/pantry/pantry.cpp
+++ src/pantry/pantry.cpp
@@ -1,26 +1,82 @@
+
+#include <iomanip>
 #include <iostream>
 #include <unordered_map> //działa szybciej od mapy (nie trzeba sortować)
 #include <fstream>
+#include <set>
 using namespace std;
 class Pantry{
     struct product{
         std::string name;
         int quantity;
-        product(std::string n = "", int q = 0){ //pusty konstruktor, int -> unsigned int, nei może byc ujemne quantity
+        std::string unit;
+        set<string> allergen;
+        public:
+        product(std::string n = "", int q = 0, std::string u = "", set<string> a = {} ){
+            if(q < 0){
+                q = 0;
+            }
+            if(u != "ml" && u != "gram" && u != "szt" && u != ""){
+                cout << "Nieprawidłowa jednostka(ml/gram/szt)" << endl;
+                return;
+            }
             name = n;
             quantity = q;
-            //jednostka miary(ml, gram, szt) -> enum
-            //alergeny, set stringów, siarczany i inne
+            unit = u;//jednostka miary(ml, gram, szt) -> enum, nie czaje co z tym enumem
+            allergen = a;
+        }
 
+        std::string get_name(){
+            return name;
+        }
+        int get_quantity(){
+            return quantity;
+        }
+        std::string get_unit(){
+            return unit;
+        }
+        set<string> get_allergen(){
+            return allergen;
+        }
+
+        void set_name(std::string n){
+            name = n;
+        }
+        void set_quantity(int q){
+            if(q < 0){
+                q = 0;
+            }
+            quantity = q;
+        }
+        void set_unit(std::string u){
+            if(u != "ml" && u != "gram" && u != "szt"){
+                cout << "Nieprawidłowa jednostka(ml/gram/szt)" << endl;
+                return;
+            }
+            unit = u;
+        }
+        void set_allergen(set<string> a){
+            allergen = a;
+        }
+
+        void print_product(){
+            cout << "Nazwa: " << name << endl;
+            cout << "Ilość: " << quantity << " " << unit << endl;
+            cout << "Alergeny: ";
+            for(auto i : allergen){ //jak pozbyć się przecinka na koniec ?
+                cout << i << ", ";
+            }
+            cout << "\n\n";
         }
 
     };
+
     unordered_map<std::string, product> shell; // unordered_map<product, >
     public:
 
-    void add_product(std::string name, int quantity){//można użyć do odejmowania qiantity
+    void add_product(std::string name, int quantity, std::string unit, set<string> allergen){//żeby patrzyło też czy laergen jest
         if (shell.find(name) == shell.end()){
-            shell[name] = product(name, quantity);
+            shell[name] = product(name, quantity, unit, allergen);
         }
         else{
             if (shell[name].quantity + quantity < 0){
@@ -31,8 +87,8 @@ class Pantry{
             }
     }
     }
-    void change_quantity(std::string name, int quantity){
-        add_product(name, -quantity);
+    void remove_quantity(std::string name, int quantity, std::string unit, set<string> allergen){
+        add_product(name, -quantity, unit, allergen);
     }
 
     void remove_product(std::string name){
@@ -48,36 +104,64 @@ class Pantry{
     }
     void print_products(){
         for(auto it = shell.begin(); it != shell.end(); it++){
-            cout << it->first << " " << it->second.quantity << " " << endl;
+            it->second.print_product();
         }
     }
 
-    void print_product(std::string name){
-        cout <<"name :" << shell[name].name << "\n" << "quantity :" << shell[name].quantity << endl;
+    void print_allergens(){
+        for(auto it = shell.begin(); it != shell.end(); it++){
+            cout << it->first << " ";
+            for(auto it2 = it->second.allergen.begin(); it2 != it->second.allergen.end(); it2++){
+                cout << *it2 << " ";
+            }
+            cout << endl;
+        }
     }
 
-
-    void write_to_file(){ // zrobic łądnie w tabelce
+    void write_to_file(){
         ofstream file;
-        file.open("pantry_base.txt"); //csv lepiej
+        file.open("pantry_base.csv", ios::out);
+        if (file.is_open()){
         for(auto it = shell.begin(); it != shell.end(); it++){
-            file << it->first << " " << it->second.quantity << endl;
+            file << it->first << "," << it->second.quantity << "," << it->second.unit;
+            for(auto it2 = it->second.allergen.begin(); it2 != it->second.allergen.end(); it2++){
+                file <<"," << *it2;
+            }
+            file << endl;
+        }
         }
         file.close();
     }
-};
-//generuj raport
-int main(){
-    Pantry p;
-    p.add_product("mleko", 10);
-    p.add_product("jajka", 20);
-    p.add_product("kakao", 10);
-    p.add_product("mleko", 10);
-    p.add_product("dżem", 10);
-    p.print_products();
-    p.add_product("dżem", -20);
-    p.print_products();
-    p.write_to_file();
 
-    return 0;
-}
+    void read_from_file_csv(){
+        ifstream file;
+        file.open("pantry_base.csv", ios::in);
+        if (file.is_open()){
+        string line;
+        while(getline(file, line)){
+            string name;
+            int quantity;
+            string unit;
+            set<string> allergen;
+            std::string temp_s;
+            stringstream ss(line);
+            getline(ss, name, ',');
+            getline(ss, temp_s, ',');
+            getline(ss, unit, ',');
+            string allergen_string;
+            while(getline(ss, allergen_string, ',')){
+                allergen.insert(allergen_string);
+            }
+            quantity = stoi(temp_s);
+            add_product(name, quantity, unit, allergen);
+        }
+        }
+        file.close();
+    }
+
+    void generate_pretty_raport(){
+        return;
+    }
+};
+
+
