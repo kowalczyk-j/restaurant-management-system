d9c8cdfd00614214a6ed8eeb8850e3f5d63e43d9
kswidere <01169249@pw.edu.pl>
2022-05-28 15:32:58 +0200

    Add setters

--- src/employees/Deliverer.h
+++ src/employees/Deliverer.h
@@ -10,6 +10,8 @@ public:
               unsigned int e_id, unsigned int r_id, Money sal) :
               Employee(fname, lname, e_id, r_id, sal) {};
 
+    void new_tip(double tip) {tips += tip;}
+
     friend std::ostream& operator<<(std::ostream& os, Deliverer deli)
     {
         deli.print(os);
