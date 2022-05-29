e3a97a6b3d148262fd70ce74ca0eb34c58e581c4
Kinga Świderek <kingaswiderek@gmail.com>
2022-05-29 16:46:12 +0200

    Dishes now added to menu in dish_type order

--- src/dish_menu/Menu.cpp
+++ src/dish_menu/Menu.cpp
@@ -4,6 +4,22 @@
 #include "Menu.h"
 #include <json/json.h>
 
+static bool compare_dish_type(Dish left, Dish right)
+{
+    return (left.get_enum_dish_type() < right.get_enum_dish_type());
+}
+
+static bool operator==(Dish left, dish_type right)
+{
+    return (left.get_enum_dish_type() == right);
+}
+
+Menu::Menu(std::vector<Dish> dishes)
+{
+    sort(dishes.begin(), dishes.end(), compare_dish_type);
+    this->dishes = dishes;
+}
+
 std::ostream& operator<<(std::ostream& os, Menu const& menu)
 {
     os << "---Menu---\n";
@@ -11,7 +27,7 @@ std::ostream& operator<<(std::ostream& os, Menu const& menu)
     // {
     //     os << menu.get_dishes()[i];
     // }
-    for(auto const &dish : menu)    //dlaczego nie działa?
+    for(auto const &dish : menu.get_dishes())    //dlaczego nie działa?
     {
         os << dish << endl;
     }
@@ -19,7 +35,8 @@ std::ostream& operator<<(std::ostream& os, Menu const& menu)
 }
 void Menu::add_dish(Dish const& dish)
 {
-    dishes.push_back(dish);
+    auto place = find(dishes.begin(), dishes.end(), dish.get_enum_dish_type());
+    dishes.insert(place, dish);
 }
 void Menu::remove_dish(unsigned int dish_idx)
 {
@@ -42,32 +59,32 @@ Json::Value Menu::parse_menu_to_json()
     }
     return root;
 }
-Menu::iterator::iterator(std::vector<Dish>::const_iterator const &cstart,
- std::vector<Dish>::const_iterator const &cstop) : start(cstart), stop(cstop)
-{
-    auto type_less = [](Dish const &left, Dish const &right)
-    { return left.get_enum_dish_type() < right.get_enum_dish_type(); };
-    current = std::min_element(start, stop, type_less);
-}
+// Menu::iterator::iterator(std::vector<Dish>::const_iterator const &cstart,
+//  std::vector<Dish>::const_iterator const &cstop) : start(cstart), stop(cstop)
+// {
+//     auto type_less = [](Dish const &left, Dish const &right)
+//     { return left.get_enum_dish_type() < right.get_enum_dish_type(); };
+//     current = std::min_element(start, stop, type_less);
+// }
 
-static bool operator==(Dish const& dish, int dish_type)
-{
-    return dish.get_enum_dish_type() == dish_type;
-}
+// static bool operator==(Dish const& dish, int dish_type)
+// {
+//     return dish.get_enum_dish_type() == dish_type;
+// }
 
-Menu::iterator Menu::iterator::operator++()
-{
-    int cur_val = current->get_enum_dish_type();
-    current = find(current+1, stop, cur_val);
-    if(current == stop) // if no more items of previous dish_type
-    {
-        if(cur_val <= napoje)
-        {
-            cur_val++;
-            current = find(start, stop, cur_val);
-        }
-        else
-            current = stop;
-    }
-    return *this;
-}
+// Menu::iterator Menu::iterator::operator++()
+// {
+//     int cur_val = current->get_enum_dish_type();
+//     current = find(current+1, stop, cur_val);
+//     if(current == stop) // if no more items of previous dish_type
+//     {
+//         if(cur_val <= napoje)
+//         {
+//             cur_val++;
+//             current = find(start, stop, cur_val);
+//         }
+//         else
+//             current = stop;
+//     }
+//     return *this;
+// }
--- src/dish_menu/Menu.h
+++ src/dish_menu/Menu.h
@@ -9,43 +9,42 @@ class Menu
     std::vector<Dish> dishes;
 
 public:
-    std::vector<Dish> & get_dishes() {return dishes;}
+    Menu(std::vector<Dish> dishes);
+    std::vector<Dish> const& get_dishes() const {return dishes;}
     void set_dishes(std::vector<Dish> d) { dishes = d; }
     size_t size() const { return dishes.size(); }
-    Menu(std::vector<Dish> di) : dishes(di)
-    {}
     void add_dish(Dish const& dish);
     void remove_dish(unsigned int dish_idx);
 
     Json::Value parse_menu_to_json();
 
-    class iterator
-    {
-        std::vector<Dish>::const_iterator current;
-        std::vector<Dish>::const_iterator start;
-        std::vector<Dish>::const_iterator stop;
-    public:
-        iterator(std::vector<Dish>::const_iterator const &cstart,
-                std::vector<Dish>::const_iterator const &cstop);
-        bool operator==(iterator const& rt) const
-        {
-            return (current == rt.current);
-        }
-        bool operator!=(iterator const& rt) const
-        {
-            return not operator==(rt);
-        }
-        iterator operator++();
-        Dish const& operator*() const
-        {
-            return *current;
-        }
-    };
-    iterator begin() const {
-        return iterator(dishes.cbegin(), dishes.cend());
-    }
-    iterator end() const {
-        return iterator(dishes.cbegin(), dishes.cend());
-    }
+    // class iterator
+    // {
+    //     std::vector<Dish>::const_iterator current;
+    //     std::vector<Dish>::const_iterator start;
+    //     std::vector<Dish>::const_iterator stop;
+    // public:
+    //     iterator(std::vector<Dish>::const_iterator const &cstart,
+    //             std::vector<Dish>::const_iterator const &cstop);
+    //     bool operator==(iterator const& rt) const
+    //     {
+    //         return (current == rt.current);
+    //     }
+    //     bool operator!=(iterator const& rt) const
+    //     {
+    //         return not operator==(rt);
+    //     }
+    //     iterator operator++();
+    //     Dish const& operator*() const
+    //     {
+    //         return *current;
+    //     }
+    // };
+    // iterator begin() const {
+    //     return iterator(dishes.cbegin(), dishes.cend());
+    // }
+    // iterator end() const {
+    //     return iterator(dishes.cbegin(), dishes.cend());
+    // }
     friend std::ostream &operator<<(std::ostream &os, Menu const &menu);
 };
